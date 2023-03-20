67048cc5934f9cc2d73e7d732c5fface9db24883
fake_state <ntdako@gmail.com>
2023-03-21 01:19:28 +0400

    add Fill New Array and Print New array

--- Program.cs
+++ Program.cs
@@ -20,17 +20,55 @@ void FillArr(string[] arr) // Заполнение массива
     }
 }
 
+void PrintArr(string[] arr, string text) // Вывод массива
+{
+    Console.WriteLine(text);
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} ");
+    }
+    Console.WriteLine(" ]");
+}
 
+string[] NewArr(string[] oldArr, string[] newArr) // Заполняем новый массив
+{
+    int i = 0;
+    int j = 0;
+    while(i < oldArr.Length)
+    {
+        string checkValue = oldArr[i];
+        if(checkValue.Length <= 3)
+        {
+            newArr[j] = oldArr[i];
+            j++;
+        }
+        i++;
+    }
+    Array.Resize(ref newArr, j);
+    return newArr;
+}
 
 Console.WriteLine("Задайте длинну массива, значения, а я сделаю массив из значений, где 3 или меньше символов.");
 // Задаем длинну массива:
-int number = NumberFromUser("Сколько строк будет в массиве? > ");
+int number = NumberFromUser("Сколько элементов будет в массиве? > ");
 
 // Проверяем, что число >= 1:
 if(PositiveNumber(number))
 Console.WriteLine("В массиве не должно быть меньше одного значения!");
 
+// Создаем два массива заданной длинны:
 string[] array = new string[number];
+string[] newArray = new string[number];
 
 // Пользователь заполняет массив:
 FillArr(array);
+
+// Вывод заданного массива на экран:
+PrintArr(array, "Заданный массив:");
+
+// Заполняем новый массив и отсекаем пустые значения:
+newArray = NewArr(array, newArray);
+
+// Печатаем новый массив:
+PrintArr(newArray, "Результат:");
